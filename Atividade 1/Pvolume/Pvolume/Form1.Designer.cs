12d47b2754fd73d09e87532675ffc7ebe88ec253
Evrtn10 <126294793+Evrtn10@users.noreply.github.com>
2023-03-24 20:23:04 -0300

    Add files via upload

---
+++ Atividade 1/Pvolume/Pvolume/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Pvolume
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRaio = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.txtRaio = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblRaio
+            // 
+            this.lblRaio.AutoSize = true;
+            this.lblRaio.Location = new System.Drawing.Point(264, 67);
+            this.lblRaio.Name = "lblRaio";
+            this.lblRaio.Size = new System.Drawing.Size(29, 13);
+            this.lblRaio.TabIndex = 0;
+            this.lblRaio.Text = "Raio";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(261, 116);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(34, 13);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "Altura";
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Location = new System.Drawing.Point(257, 171);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(42, 13);
+            this.lblresultado.TabIndex = 2;
+            this.lblresultado.Text = "Volume";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(288, 238);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // btnsair
+            // 
+            this.btnsair.Location = new System.Drawing.Point(413, 238);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(75, 23);
+            this.btnsair.TabIndex = 4;
+            this.btnsair.Text = "Fechar";
+            this.btnsair.UseVisualStyleBackColor = true;
+            this.btnsair.Click += new System.EventHandler(this.Btnsair_Click);
+            // 
+            // txtRaio
+            // 
+            this.txtRaio.Location = new System.Drawing.Point(335, 67);
+            this.txtRaio.Name = "txtRaio";
+            this.txtRaio.Size = new System.Drawing.Size(100, 20);
+            this.txtRaio.TabIndex = 5;
+            this.txtRaio.Validated += new System.EventHandler(this.TxtRaio_Validated);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(335, 116);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 6;
+            this.txtAltura.Validated += new System.EventHandler(this.TxtAltura_Validated);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Location = new System.Drawing.Point(335, 164);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtRaio);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblRaio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRaio;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.TextBox txtRaio;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ Atividade 1/Pvolume/Pvolume/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pvolume
+{
+    public partial class Form1 : Form
+    {
+        double raio, altura, volume; /*globais */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnsair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            if ((!double.TryParse(txtRaio.Text, out raio)) || 
+                    (!double.TryParse(txtAltura.Text, out altura)))
+                {
+                MessageBox.Show("Valores Inválidos");
+            }
+            else
+            {
+                if ((altura<=0) || (raio<=0))
+                {
+                    MessageBox.Show("Valores não podem ser <=0");
+                }
+                else
+                {
+                    volume = Math.PI * Math.Pow(raio, 2) * altura;
+                    txtResultado.Text = volume.ToString("N2");
+                }
+            }
+        }
+
+        private void TxtRaio_Validated(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtRaio.Text, out raio))
+            {
+                MessageBox.Show("Raio Inválido");
+            }
+        }
+
+        private void TxtAltura_Validated(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtAltura.Text, out altura))
+            {
+                MessageBox.Show("Altura Inválido");
+            }
+            else
+                if (altura <=0)
+            {
+                MessageBox.Show("Altura deve ser maior que zero"); //textBox2.focus();
+            }
+        }
+    }
+}
